395099bb4ef558fac5b06a24f16c3b63d96eb649
Kevin Ulliac <kevanoullio@outlook.com>
2025-12-23 13:52:07 -0700

    Consolidated all global css to index.css and organized it more appropriately.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Footer } from "@/components/Footer"
 import { Home } from "@/pages/Home"
 import { Projects } from "@/pages/Projects"
 import { Contact } from "@/pages/Contact"
-import "./App.css"
 
 function App() {
   return (
